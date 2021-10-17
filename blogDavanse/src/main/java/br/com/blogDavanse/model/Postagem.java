8c6bd64c48bf228bda70ff786682f385c7fc985a
davansep <priscila.davanse@gmail.com>
2021-10-17 15:36:25 -0300

    [Task: CREATE relationship between tables] 17.10.2021

--- blogDavanse/src/main/java/br/com/blogDavanse/model/Postagem.java
+++ blogDavanse/src/main/java/br/com/blogDavanse/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * @author Priscila Davanse
@@ -34,7 +37,11 @@ public class Postagem {
 
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
-
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public Long getId() {
 		return id;
 	}
@@ -67,4 +74,11 @@ public class Postagem {
 		this.date = date;
 	}
 
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 }
