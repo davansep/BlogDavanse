eb4faba867c59e15cbe3ac5148bde30b922fb73b
davansep <priscila.davanse@gmail.com>
2021-10-17 12:44:18 -0300

    [commit initial] 17.10.2021

---
+++ blogDavanse/src/main/java/br/com/blogDavanse/BlogDavanseApplication.java
@@ -0,0 +1,13 @@
+package br.com.blogDavanse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDavanseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDavanseApplication.class, args);
+	}
+
+}
---
+++ blogDavanse/src/test/java/br/com/blogDavanse/BlogDavanseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blogDavanse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDavanseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
