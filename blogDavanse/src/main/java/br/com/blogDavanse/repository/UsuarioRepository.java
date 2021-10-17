d98a53f5b4dc2058f96a0b723558021e329bded9
davansep <priscila.davanse@gmail.com>
2021-10-17 19:52:41 -0300

    [Task: CREATE interface UsuarioRepository] 17.10.2021

---
+++ blogDavanse/src/main/java/br/com/blogDavanse/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package br.com.blogDavanse.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.blogDavanse.model.Usuario;
+
+/**
+ * @author Priscila Davanse
+ * @version 1.0
+ */
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	public Optional<Usuario> findByEmail(String email);
+}
