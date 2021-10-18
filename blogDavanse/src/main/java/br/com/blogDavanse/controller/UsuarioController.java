fc4fede1718413e21b60042722aca84d5132612c
davansep <priscila.davanse@gmail.com>
2021-10-18 15:09:49 -0300

    [Task: MODIFY classes] 18.10.2021

--- blogDavanse/src/main/java/br/com/blogDavanse/controller/UsuarioController.java
+++ blogDavanse/src/main/java/br/com/blogDavanse/controller/UsuarioController.java
@@ -1,13 +1,27 @@
 package br.com.blogDavanse.controller;
 
+import java.util.List;
 import java.util.Optional;
+
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.blogDavanse.model.Postagem;
 import br.com.blogDavanse.model.UserLogin;
 import br.com.blogDavanse.model.Usuario;
+import br.com.blogDavanse.repository.UsuarioRepository;
 import br.com.blogDavanse.service.UsuarioService;
 
 /**
@@ -21,6 +35,24 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Usuario>> getByNome(@PathVariable String nome) {
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
 
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> autentication(@Valid @RequestBody Optional<UserLogin> user) {
@@ -31,5 +63,15 @@ public class UsuarioController {
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> post(@Valid @RequestBody Usuario novoUsuario) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.CadastrarUsuario(novoUsuario));
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> put(@Valid @RequestBody Usuario novoUsuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(novoUsuario));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
--- blogDavanse/src/main/java/br/com/blogDavanse/repository/UsuarioRepository.java
+++ blogDavanse/src/main/java/br/com/blogDavanse/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.com.blogDavanse.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +14,6 @@ import br.com.blogDavanse.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	public Optional<Usuario> findByEmail(String email);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 }
